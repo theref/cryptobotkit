694a80cf3e1c7d8244a0c4ec9a3b895ee1d27256
Owen Campbell <owen.campbell@tanti.org.uk>
2019-11-05 10:59:41 +0000

    Add fund to test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,7 @@
-from cryptobotkit.model import Exchange
+from cryptobotkit.model import Exchange, Fund
 import pytest
 
 def test_missing_attributes():
     with pytest.raises(TypeError):
         exchange = Exchange()
+        fund = Fund()
