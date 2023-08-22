286d9231664fec0421d59ddb8f8f3dceb241f7b9
Sourcery AI <>
2023-08-22 10:07:43 +0000

    'Refactored by Sourcery'

--- cryptobotkit/ccxt-test.py
+++ cryptobotkit/ccxt-test.py
@@ -9,7 +9,7 @@ import ccxt  # noqa: E402
 # -----------------------------------------------------------------------------
 
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(root + "/python")
+sys.path.append(f"{root}/python")
 
 
 binance = ccxt.binanceje()
